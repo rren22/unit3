10ea2ffb23daa56fff1123d60a9a72e5b2ae837b
rren22 <rren22@wisc.edu>
2026-03-19 16:35:41 -0500

    fixing paths and axes

--- bubblechart.js
+++ bubblechart.js
@@ -18,17 +18,17 @@ d3.csv("data/MegaCities.csv").then(function(data) {
     });
 
     // scales
-    var xScale = d3.scaleLinear()
+        var xScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.Pop_1985)])
-        .range([50, width - 50]);
+        .range([80, width - 80]);
 
     var yScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.Pop_2015)])
-        .range([height - 50, 50]);
+        .range([height - 80, 80]);
 
     var rScale = d3.scaleSqrt()
         .domain([0, d3.max(data, d => d.Pop_2015)])
-        .range([5, 40]);
+        .range([5, 25]); // smaller bubbles
 
     // draw bubbles
     svg.selectAll("circle")
@@ -39,16 +39,29 @@ d3.csv("data/MegaCities.csv").then(function(data) {
         .attr("cy", d => yScale(d.Pop_2015))
         .attr("r", d => rScale(d.Pop_2015))
         .attr("fill", "steelblue")
-        .attr("opacity", 0.7);
+        .attr("opacity", 0.6);
 
-    // labels (optional but helpful)
+    // labels (slightly shifted to reduce overlap)
     svg.selectAll("text")
         .data(data)
         .enter()
         .append("text")
         .attr("x", d => xScale(d.Pop_1985))
-        .attr("y", d => yScale(d.Pop_2015))
+        .attr("y", d => yScale(d.Pop_2015) - 8)
         .text(d => d.City)
-        .attr("font-size", "10px");
-});
+        .attr("font-size", "9px")
+        .attr("text-anchor", "middle");
 
+    // axes
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${height - 80})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("transform", `translate(80, 0)`)
+        .call(yAxis);
+
+});
